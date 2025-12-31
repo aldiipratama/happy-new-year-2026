a705cb79161d27b0b7a001fd69a73ebbaec66d6a
aldiipratama <paldi0013@gmail.com>
2025-12-31 19:53:26 +0700

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { render } from 'preact'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import { App } from './app'
+import { render } from "preact";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { App } from "./app";
+import { Analytics } from "@vercel/analytics/react";
 
 render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
-  document.getElementById('app')!
-)
+  document.getElementById("app")!
+);
